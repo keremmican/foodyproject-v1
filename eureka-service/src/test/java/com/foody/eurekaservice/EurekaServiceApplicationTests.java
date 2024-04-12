6625f94ad6867bd8cb99a83a3713011ede10a39b
keremmican <keremmican@hotmail.com>
2024-04-12 22:47:51 +0300

    initial

---
+++ eureka-service/src/test/java/com/foody/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foody.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ food-service/src/test/java/com/foody/foodservice/FoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foody.foodservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
